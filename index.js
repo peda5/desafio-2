a67eb357043d1a0005bfb98c3577564fdb564be7
peda5 <pedrokiagojunior23@gmail.com>
2024-10-15 21:17:32 -0300

    Desafio 2 DIO

---
+++ index.js
@@ -0,0 +1,24 @@
+
+calculadoraDeRank(89,45)
+function calculadoraDeRank(vitorias, derrotas){
+        let nivel = "";
+        let saldoVitorias = vitorias - derrotas
+       if(vitorias < 10)
+        nivel = "Ferro";
+       else if( vitorias > 11 && vitorias < 20)
+        nivel = "Bronze";
+       else if ( vitorias > 21 && vitorias < 50)
+        nivel = "Prata"
+       else if( vitorias > 51 && vitorias < 80)
+        nivel = "Ouro"
+       else if ( vitorias > 81 && vitorias < 90)
+        nivel = "Diamante";
+       else if ( vitorias > 91 && vitorias < 100)
+        nivel = "Lendário";
+       else if (vitorias < 101)
+       nivel = "Imortal"
+       console.log(`O herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`)
+       
+    }
+
+
